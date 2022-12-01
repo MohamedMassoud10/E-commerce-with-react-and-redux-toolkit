45d834bd55dfd890f66dd5cb36c1e35b442dac52
MohamedMassoud10 <massoudfcai@gmail.com>
2022-12-01 21:40:59 +0200

    jsone

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react'
 import '../index.css'
 import { NavLink } from 'react-router-dom'
 import CartContent from '../pages/CartContent'
-import { useSelector } from 'react-redux'
+//import { useSelector } from 'react-redux'
 const Navbar = ({productCount}) => {
   let [showCart,setShowCart]=React.useState(false);
-  let {carts}=useSelector()
+  //let {carts}=useSelector()
  // console.log(carts)
   function handleClick(){
     setShowCart((prevent=>!prevent))
