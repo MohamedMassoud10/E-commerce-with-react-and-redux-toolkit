ff860085500d935b041bd6db36c562f71d62dc8a
MohamedMassoud10 <massoudfcai@gmail.com>
2022-12-04 22:45:44 +0200

    Data

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setProductsData(data));
     setLoading(true);
   }, []);
   return (
--- src/components/Collections.js
+++ src/components/Collections.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Cart from './cart'
-import LoadingPage from '../pages/LoadingPage'
-const Collections = ({productsData , Loading}) => {console.log(productsData)
+import React from "react";
+import Cart from "./cart";
+import LoadingPage from "../pages/LoadingPage";
+const Collections = ({ productsData, Loading }) => {
+  console.log(productsData);
   return (
     <div className="container">
-    <div className='carts-list'>
-      {productsData.map(e=>{
-    return(
-    <Cart img={e.image} title={e.title} price={e.price} id={e.id} key={e.id}/>
-    )
-  })}
+      <div className="carts-list">
+        {gitproductsData.map((e) => {
+          return (
+            <Cart
+              img={e.image}
+              title={e.title}
+              price={e.price}
+              id={e.id}
+              key={e.id}
+            />
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Collections;
